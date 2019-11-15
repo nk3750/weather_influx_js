5a606546521305863e2be1770da9858d94449a9b
Neelabh <nk3750@rit.edu>
2019-11-15 16:33:38 -0500

    Displaying graphs for both the queries

--- client/src/views/CurrentWeather.js
+++ client/src/views/CurrentWeather.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { default as fetch } from 'isomorphic-fetch';
 import { JsonToTable } from "react-json-to-table";
-const JsonTable = require('ts-react-json-table');
+import { LineChart, Line,CartesianGrid,XAxis,YAxis,Tooltip } from 'recharts';
 class CurrentWeather extends Component {
 
   constructor(props){
@@ -27,10 +27,29 @@ class CurrentWeather extends Component {
   }
 
   render() {
+    var js0=this.state.data[0];
+    let prop=[];
+    for ( var property in this.state.data[0] ) {
+        prop.push(property)
+      }
+    console.log(prop);
     return(
       <div>
         
         <JsonToTable json = {this.state.data} />
+        <LineChart width={400} height={400} data={this.state.data}>
+    <Line type="monotone" dataKey={prop[3]} stroke="#8884d8" />
+    <Line type="monotone" dataKey={prop[4]} stroke="#FF3366" />
+    <Line type="monotone" dataKey={prop[5]} stroke="#CCFF33" />
+    <Line type="monotone" dataKey={prop[6]} stroke="#CC0099" />
+    <Line type="monotone" dataKey={prop[7]} stroke="#330066" />
+    <Line type="monotone" dataKey={prop[8]} stroke="#0000CC" />
+
+    <CartesianGrid stroke="#ccc" />
+    <XAxis dataKey={prop[0]} />
+    <YAxis />
+    <Tooltip />
+  </LineChart>
     
     </div>
     )
